86a2eb1e661eb09d850dd4cdd375796310618802
Kaushal more <kaushalrmore9403@gmail.com>
2025-08-07 14:24:41 +0530

    Update ContactsController.cs

--- ContactManagementSystem/Views/ContactsController.cs
+++ ContactManagementSystem/Views/ContactsController.cs
@@ -24,7 +24,6 @@ namespace ContactManagementSystem.Views
         {
             return View(await _context.Contact.ToListAsync());
         }
-
         // GET: Contacts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
